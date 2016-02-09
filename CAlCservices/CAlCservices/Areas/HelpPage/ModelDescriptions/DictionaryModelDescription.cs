5c12f2c3d38d502ce3c04ef23cd85924c87dd0df
Singh, Bharat <bharat.singh@accenture.com>
2016-02-09 17:55:12 +0530

    createing setup

---
+++ CAlCservices/CAlCservices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CAlCservices.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CAlCservices/CAlCservices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CAlCservices.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
